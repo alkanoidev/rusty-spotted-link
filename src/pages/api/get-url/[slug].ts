3d2b5d1f958a23c0bce3a7c26e12ad35d9ae79a3
Filip Ivanovic <alkanoidev@gmail.com>
2022-06-27 15:02:50 +0200

    getting and redirecting with a slug

---
+++ src/pages/api/get-url/[slug].ts
@@ -0,0 +1,25 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../db/client";
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const slug = req.query["slug"];
+
+  if (!slug || typeof slug != "string") {
+    res.status(404).json({ message: "slug not found" });
+    return;
+  }
+
+  const data = await prisma.shortLink.findFirst({
+    where: {
+      slug: {
+        equals: slug,
+      },
+    },
+  });
+
+  if (!data) {
+    res.status(404).json({ message: "slug does not exist" });
+    return;
+  }
+
+  return res.redirect(data.url);
+};
