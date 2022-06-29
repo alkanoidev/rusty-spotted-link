71e6244a4cb30f4c03d134c04e53988b5030b555
Filip Ivanovic <alkanoidev@gmail.com>
2022-06-29 20:16:07 +0200

    fixed success page, removed icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
 import type { NextPage } from "next";
-import Image from "next/image";
-import Logo from "../assets/logo.svg";
 import Head from "next/head";
 import React, { useEffect, useRef, useState } from "react";
 import axios from "axios";
@@ -56,31 +54,9 @@ const Home: NextPage = () => {
   return (
     <div className="h-full w-full flex flex-col gap-5 items-center pt-44 justify-center">
       <Head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/root/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/root/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/root/favicon-16x16.png"
-        />
-        <link rel="icon" href="/root/favicon.ico" />
-        <link rel="manifest" href="/root/manifest.json" />
-        <link rel="icon" href="/root/favicon.png" />
-
         <title>Rusty Spotted Link</title>
       </Head>
 
-      <Image src={Logo} width={70} height={70} />
       <h1>Rusty Spotted Link</h1>
       <form
         method="post"
@@ -101,6 +77,7 @@ const Home: NextPage = () => {
             type="text"
             name="url"
             id="url"
+            placeholder="https://..."
             ref={urlRef}
           />
         </p>
@@ -118,6 +95,7 @@ const Home: NextPage = () => {
             type="text"
             name="slug"
             id="slug"
+            placeholder="my-path"
             ref={slugRef}
           />
         </p>
--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,12 +1,20 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import { useRouter, withRouter } from "next/router";
-import Logo from "../assets/logo.svg";
-import Image from "next/image";
+import { useRouter } from "next/router";
 
-const Success: NextPage = ({}) => {
+type Props = {
+  host: string | null;
+};
+
+export const getServerSideProps: GetServerSideProps<Props> = async (
+  context
+) => ({
+  props: { host: context.req.headers.host || null },
+});
+
+const Success: NextPage<Props> = ({ host }) => {
   const [url, setUrl] = useState("");
   const [slug, setSlug] = useState("");
   const router = useRouter();
@@ -24,41 +32,44 @@ const Success: NextPage = ({}) => {
   return (
     <div className="h-full w-full flex flex-col gap-5 items-center pt-44 justify-center">
       <Head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/root/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/root/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/root/favicon-16x16.png"
-        />
-        <link rel="icon" href="/root/favicon.ico" />
-        <link rel="manifest" href="/root/manifest.json" />
-        <link rel="icon" href="/root/favicon.png" />
-
         <title>Rusty Spotted Link</title>
       </Head>
-      <Image src={Logo} width={70} height={70} />
+
       <p className="text-center text-dark dark:text-light text-xl">
-        Your Rusty Spotted Link for <Link href={url}>{url.toString()}</Link> was
-        created! <br />
-        You can access it at{" "}
-        <Link href={router.basePath + "/" + slug}>
-          {router.basePath + "/" + slug}
-        </Link>
+        Your Rusty Spotted Link was created!
       </p>
       <h1 className="text-8xl">🎉</h1>
+      <div className="p-4 flex text-dark dark:text-light flex-col gap-4 rounded-lg w-full sm:w-[400px]">
+        <label>Your long url:</label>
+        <div
+          className="rounded w-full overflow-x-auto flex py-3 px-2
+                    bg-off-white dark:bg-off-dark text-dark dark:text-light"
+        >
+          <Link href={url}>{url.toString()}</Link>{" "}
+        </div>
+
+        <label>Your rusty spotted link:</label>
+
+        <div
+          className="rounded w-full overflow-x-auto flex py-3 px-2
+                    bg-off-white dark:bg-off-dark text-dark dark:text-light"
+        >
+          <Link href={host + "/" + slug}>{host + "/" + slug}</Link>
+        </div>
+
+        <button
+          className="transition capitalize w-full border relative border-primary rounded text-lg text-dark dark:text-light py-2 
+                       z-0 after:w-full after:h-full after:bg-primary after:top-0 after:left-0 after:absolute 
+                       after:z-[-1] after:opacity-50 after:transition hover:after:opacity-80 font-semibold"
+          onClick={() => {
+            router.replace("/");
+          }}
+        >
+          Make another short link
+        </button>
+      </div>
     </div>
   );
 };
 
-export default withRouter(Success);
+export default Success;
